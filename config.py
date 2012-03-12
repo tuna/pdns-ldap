248ce44a4e41d730fd6a5b9547a135e9fe24d4c5
Cheer Xiao <xiaqqaix@gmail.com>
2012-03-12 20:16:32 +0800

    Switched to 60s TTL

--- config.py
+++ config.py
@@ -19,7 +19,7 @@ cert = '/etc/ssl/tuna/server.crt'
 binddn = 'cn=pdns,ou=robots,o=tuna'
 bindpw = "NOT STORED IN GIT REPO"
 
-ttl = '300'
+ttl = '60'
 zones = ( 'tuna.tsinghua.edu.cn', 'tuna' )
 ns_dcs = ( 'dns', 'dns2' )
 campus = ( '59.66.0.0/16', '166.111.0.0/16' )
